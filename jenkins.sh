5a8d63598f48b1d0b088a1d5304fefb3c2e82f5b
shodhan sheth <shodhan.sheth@digital.cabinet-office.gov.co.uk>
2012-10-23 16:05:46 +0000

    Adding jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+export FACTER_govuk_platform=test
+export RAILS_ENV=test
+export DISPLAY=":99"
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake
+RESULT=$?
+exit $RESULT
