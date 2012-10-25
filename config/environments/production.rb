972d66a79264f2c076068eb4eeba8dd018112174
Mark Needham <m.h.needham@gmail.com>
2012-10-25 12:15:42 +0100

    set the delivery method for emails to SES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Errbit::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :ses
 
   # Set default host for ActionMailer links
   config.action_mailer.default_url_options = { :host => ENV['ERRBIT_HOST'] } if ENV['ERRBIT_HOST']
