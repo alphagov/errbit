9a70cc32dc5017db29ed819d308638b2b38569d1
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2015-04-10 10:40:32 +0000

    Mark test that tests disabled feature
    we're not using integration with issue trackers. see: db5a2332.

--- spec/acceptance/app_regenerate_api_key_spec.rb
+++ spec/acceptance/app_regenerate_api_key_spec.rb
@@ -72,7 +72,7 @@ feature "Create an application" do
 
   end
 
-  scenario "create an apps with issue tracker and edit it", :js => true do
+  scenario "create an apps with issue tracker and edit it", :js => true, :pending => "We've disabled integration with issue trackers, see db5a2332" do
     visit '/'
     log_in admin
     click_on I18n.t('apps.index.new_app')
